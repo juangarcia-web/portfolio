de86ccd4763bf5300e6ece570d3ea3e19f14098b
Your Name <juangarcia170593@gmail.com>
2021-03-25 20:16:54 -0600

    backround

---
+++ portafolio/assets/css/ws.js
@@ -0,0 +1,8 @@
+user = { name: 'Daniel', email: 'daniel@gmail.com', gender: 'Male' }
+return
+
+// function getEmail(user) {
+    
+// }
+
+// console.log(getEmail([{name: 'Daniel', email: 'daniel@gmail.com', gender: 'Male' }}]))
